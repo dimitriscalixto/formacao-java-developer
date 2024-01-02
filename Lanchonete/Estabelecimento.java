2dc57e369c8c9b20ef75aaf9596aedca59daf82c
idimitrisc47 <dimitrisc47@gmail.com>
2024-01-02 12:35:59 -0500

    visibilidade de recursos

---
+++ Lanchonete/Estabelecimento.java
@@ -0,0 +1,41 @@
+package Lanchonete;
+
+import Lanchonete.area.cliente.Cliente;
+import Lanchonete.atendimento.Atendente;
+import Lanchonete.atendimento.cozinha.Almoxarife;
+import Lanchonete.atendimento.cozinha.Cozinheiro;
+
+public class Estabelecimento {
+    public static void main(String[] args) {
+        Cozinheiro cozinheiro = new Cozinheiro();
+
+        //ações que estabelecimento precisa ter ciência
+        cozinheiro.adicionarSucoNoBalcao();
+        cozinheiro.adicionarLancheNoBalcao();
+        cozinheiro.adicionarComboNoBalcao();
+
+        Almoxarife almoxarife = new Almoxarife();
+        //ação que somente o seu pacote cozinha precisa conhecer (default)
+        almoxarife.entregarIngredientes();
+        almoxarife.trocarGas();
+
+        Atendente atendente = new Atendente();
+        atendente.pegarLancheCozinha();
+        atendente.receberPagamento();
+        atendente.servindoMesa();
+
+
+        Cliente cliente = new Cliente();
+        cliente.escolherLanche();
+        cliente.fazerPedido();
+        cliente.pagarConta();
+
+        //não deveria, mas o estabelecimento
+        //ainda não definiu normas de atendimento
+        cliente.pegarPedidoBalcao();
+
+        //esta ação é muito sigilosa, qua tal ser privada ?
+        cliente.consultarSaldoAplicativo();
+
+    }
+}
